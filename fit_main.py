cc6556a5d0e31306e2d33951cd5fe070914e2968
Panadestein <rpana92@gmail.com>
2018-06-12 21:17:17 +0200

    License and authorship

--- fit_main.py
+++ fit_main.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2018 AGUILAR-MENA Jimmy and PANADES-BARRUETA Ramon L.
+# This file is part of capi_nlopt <https://github.com/Panadestein/capi_nlopt.git> 
+#
+# capi_nlopt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# capi_nlopt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with capi_nlopt.  If not, see <http://www.gnu.org/licenses/>.
 #!/usr/bin/env python3
 
 import numpy as np
